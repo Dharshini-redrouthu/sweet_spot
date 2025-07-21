e6ee81cdf6bd576ece282b9c75f6642447089c46
dharshini <dharshiniredrouthu@gmail.com>
2025-07-21 21:58:47 +0530

    initial

---
+++ App.jsx
@@ -0,0 +1,16 @@
+/*import HomePage from "./pages/HomePage";
+function App() {
+  return (
+    
+    <HomePage></HomePage>
+  );
+}
+
+export default App;*/
+import StoreManagement from './components/admin/StoreManagement'
+
+function App() {
+  return <StoreManagement />
+}
+
+export default App
